4ba0cdff64454d9976dbd9d7c026ca0b52256fd6
louie <rubinlouie41@gmail.com>
2025-02-17 21:03:11 +0800

    aa

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# This route will render the index.html file when the app is accessed at the root URL '/'
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template('index.html')  # Automatically displays index.h
 
-@app.route('/prev]')
-def profile():
-    return render_template('prev.html')  
+# Additional routes for other pages
+@app.route('/prev')
+def prev():
+    return render_template('prev.html')  # Render prev.html when accessed at /prev
 
 if __name__ == '__main__':
     app.run(debug=True)
